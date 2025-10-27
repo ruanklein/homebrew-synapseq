c0de85591d5f48620bdb0a115ec162ec42a15a08
Ruan <ruan.klein@gmail.com>
2025-10-26 22:40:31 -0300

    feat: added samples, scripts, contrib, docs, and linux support

--- Formula/synapseq.rb
+++ Formula/synapseq.rb
@@ -4,12 +4,26 @@ class Synapseq < Formula
   version "3.2.1"
   license "GPL-2.0-only"
 
-  on_arm do
-    url "https://github.com/ruanklein/synapseq/releases/download/v3.2.1/synapseq-v3.2.1-macos-arm64.tar.gz"
-    sha256 "a71aafc66cebe369fba136b4d5ed77f5a67ab10804e7a74268eaa6954f658b04"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/ruanklein/synapseq/releases/download/v3.2.1/synapseq-v3.2.1-macos-arm64.tar.gz"
+      sha256 "a71aafc66cebe369fba136b4d5ed77f5a67ab10804e7a74268eaa6954f658b04"
+    else
+      odie "SynapSeq is only available for macOS ARM64 (Apple Silicon)."
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/ruanklein/synapseq/releases/download/v3.2.1/synapseq-v3.2.1-linux-arm64.tar.gz"
+      sha256 "ae4dc5fff0aea1c447dd6a6273a552378d2d83289878dbcc71e32f619ec3b676"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/ruanklein/synapseq/releases/download/v3.2.1/synapseq-v3.2.1-linux-amd64.tar.gz"
+      sha256 "c6cc65c59c31b8fdb906fde461ce19fb0806670536dbceebec5f46a91d16e16a"
+    else
+      odie "Unsupported Linux architecture for SynapSeq."
+    end
+  else
+    odie "Unsupported operating system for SynapSeq."
   end
-
-  depends_on arch: :arm64
 
   def install
     bin_path = Dir["**/bin/synapseq*"].find { |f| File.basename(f) !~ /\.sha256$/ }
@@ -19,11 +33,33 @@ class Synapseq < Formula
     bin.install bin_path => "synapseq"
 
     manpage = Dir["**/man/synapseq.1"].first
-    if manpage
-      man1.install manpage
-    else
-      opoo "No manpage found in archive."
-    end
+    man1.install manpage if manpage
+
+    pkgshare.install "samples" if Dir.exist?("samples")
+    pkgshare.install "scripts" if Dir.exist?("scripts")
+    pkgshare.install "contrib" if Dir.exist?("contrib")
+
+     doc.install Dir["*.txt"] if Dir["*.txt"].any?
+  end
+
+  def caveats
+    <<~EOS
+      SynapSeq was successfully installed!
+
+      Additional resources were installed to:
+        #{opt_pkgshare}
+
+      The following directories are available:
+        - samples => example .spsq sessions and structured formats
+        - scripts => helper scripts and streaming examples
+        - contrib => community presets and advanced sequences
+
+      To learn how to use SynapSeq, read the manual page:
+        man synapseq
+
+      Or check out the usage documentation:
+        less #{opt_doc}/USAGE.txt
+    EOS
   end
 
   test do
